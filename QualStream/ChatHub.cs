024c8fbdf418e3f6d314e9eb8a8116b182a99a49
Simon Johansson <simon.johansson@zocom.se>
2020-02-06 13:33:14 +0100

    inital commit

---
+++ QualStream/ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace QualStream
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task SyncVideo(int timestamp)
+        {
+            await Clients.All.SendAsync("ReceiveTimestamp", timestamp);
+        }
+    }
+}
---
+++ QualStream/ClientApp/src/components/Chat.js
@@ -0,0 +1,133 @@
+import React, { Component } from 'react';
+import * as signalR from "@microsoft/signalr";
+import YouTube from 'react-youtube';
+const opts = {
+    height: '390',
+    width: '640',
+    playerVars: { // https://developers.google.com/youtube/player_parameters
+      autoplay: 1
+    }
+  };
+
+export class Chat extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            nick: '',
+            message: '',
+            messages: [],
+            hubConnection: null,
+            currentvideotime: 0,
+        };
+    }
+    componentDidMount = () => {
+        this.onYouTubeIframeAPIReady();
+        const nick = window.prompt('Your name:', 'John');
+        const hubConnection = new signalR.HubConnectionBuilder()
+            .withUrl("/chatHub")
+            .build();
+        this.setState({ hubConnection, nick }, () => {
+            this.state.hubConnection
+              .start()
+              .then(() => console.log('Connection started!'))
+              .catch(err => console.log('Error while establishing connection :('));
+              this.state.hubConnection.on("ReceiveMessage", (nick, message) => {
+         
+                const text = `${nick}: ${message}`;
+                const messages = this.state.messages.concat([text]);
+                this.setState({ messages });
+                const chatContainer = document.querySelector(".ChatContainer");
+                chatContainer.scrollTop = chatContainer.scrollHeight;
+            });
+          });
+          
+    
+    }
+    onYouTubeIframeAPIReady = () => {
+        const opts = {
+            height: '390',
+            width: '640',
+            playerVars: { // https://developers.google.com/youtube/player_parameters
+              autoplay: 1,
+              start: 0,
+              end: 0
+
+            }
+        };
+        this.setState({player: opts});
+    }
+    sendMessage = (e) => {
+        if(!this.state.nick)
+        {
+            const nick = window.prompt('Your name:', 'John');
+            this.setState({  nick });
+        }
+        this.state.hubConnection
+        .invoke('SendMessage', this.state.nick, this.state.message)
+        .catch(err => console.error(err));
+
+        this.setState({message: ''});
+ 
+    }
+    inputChanged = (e) => {
+        this.setState({ message: e.target.value });
+    }
+    onKeyPressed = (e) => {
+        if (e.which === 13 || e.keyCode === 13) {
+            this.sendMessage(e);
+        }
+    }
+    onReady = (e) => {
+        // access to player in all event handlers via event.target
+        e.target.pauseVideo();
+        setInterval(() => {
+            this.updateytplayerInfo(e);
+        }, 500);
+    }
+    onPlay = (e) => {
+        // console.log(e);
+    }
+    updateytplayerInfo = (e) => {
+        var currentvideotime = e.target.getCurrentTime();
+        
+        if (currentvideotime != this.state.currentvideotime) {
+            this.setState({ currentvideotime });
+        }
+    }
+    onStateChange = (e) => {
+       
+        const currentTime = e.target.getCurrentTime();
+      
+
+    }
+    render() {
+
+        return (
+            <div>
+                <YouTube videoId="2g811Eo7K8U"
+                    opts={opts}
+                    onReady={this.onReady}
+                    onPlay={this.onPlay}
+                    onStateChange={this.onStateChange}
+                    />
+                <div className="ChatContainer">
+                {this.state.messages.map((message, index) => (
+                  <span style={{display: 'block'}} key={index}> {message} </span>
+                ))}
+              </div>
+              <br />
+              <input
+                type="text"
+                value={this.state.message}
+                onChange={this.inputChanged}
+                onKeyDown={this.onKeyPressed}
+                tabIndex="0"
+              />
+        
+              <button onClick={this.sendMessage}>Send</button>
+        
+             
+            </div>
+          );
+    }
+}
